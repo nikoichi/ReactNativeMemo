ca528ccb50da9076f319603e59e5404d98ca17a4
Koichi Watanabe <monopori151@gmail.com>
2018-03-30 16:29:43 +0900

    create memo edit screen

---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CircleButtom from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.memoEditInput} multiline value="Hi" />
+        <CircleButtom>
+          {'\uf00c'}
+        </CircleButtom>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+  },
+});
+
+export default MemoEditScreen;
+
