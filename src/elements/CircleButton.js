df92e22616602d61bb9d034cb20247109237a85c
Koichi Watanabe <monopori151@gmail.com>
2018-03-31 14:02:27 +0900

    install react navigation

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Font } from 'expo';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
 import fontAwesome from '../../assets/fonts/fontawesome-webfont.ttf';
 
 class CircleButton extends React.Component {
@@ -18,7 +18,7 @@ class CircleButton extends React.Component {
   }
 
   render() {
-    const { style, color } = this.props;
+    const { style, color, onPress } = this.props;
 
     let bgColor = '#E31676';
     let textColor = '#fff';
@@ -29,24 +29,28 @@ class CircleButton extends React.Component {
     }
 
     return (
-      <View style={[styles.CircleButton, style, { backgroundColor: bgColor }]}>
-        {
-          this.state.fontLoaded ? (
-            <Text style={[styles.CircleButtonTitle, { color: textColor }]}>
-              {this.props.children}
-            </Text>
-          ) : null
-        }
-      </View>
+      <TouchableHighlight style={[styles.container, style]} onPress={onPress} underlayColor="transparent">
+        <View style={[styles.circleButton, { backgroundColor: bgColor }]}>
+          {
+            this.state.fontLoaded ? (
+              <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+                {this.props.children}
+              </Text>
+            ) : null
+          }
+        </View>
+      </TouchableHighlight>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  CircleButton: {
+  container: {
     position: 'absolute',
     bottom: 32,
     right: 32,
+  },
+  circleButton: {
     width: 48,
     height: 48,
     borderRadius: 24,
@@ -58,7 +62,7 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
 
-  CircleButtonTitle: {
+  circleButtonTitle: {
     fontFamily: 'FontAwesome',
     fontSize: 24,
     lineHeight: 24,
--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -7,8 +7,8 @@ class MemoListScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MemoList />
-        <CircleButton>
+        <MemoList navigation={this.props.navigation} />
+        <CircleButton onPress={() => { this.props.navigation.navigate('MemoEdit'); }}>
           {'\uf067'}
         </CircleButton>
       </View>
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
+    backgroundColor: '#FFFDF6',
   },
 });
 
