cc31e04913de696970663e6f42d5bbd7acd62cc3
Koichi Watanabe <monopori151@gmail.com>
2018-03-31 16:28:20 +0900

    firebase certificate

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,15 +2,38 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  // eslint-disable-next-line
+  hundlesSubmit() {
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           ログイン
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.button} onPress={() => { this.props.navigation.navigate('Home'); }}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitalize="none"
+          placeholder="password"
+          secureTextEntry
+        />
+        <TouchableHighlight style={styles.button} onPress={this.hundlesSubmit.bind(this)}>
           <Text style={styles.buttonTitle}>
             ログインする
           </Text>
