c719f8cf2bd94565735359d7b635ffde2afbe4d0
Koichi Watanabe <monopori151@gmail.com>
2018-04-01 09:59:49 +0900

    store memos with cloudstore

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import ENV from './env.json';
+
+// eslint-disable-next-line
+require("firebase/firestore");
 
 const config = {
   apiKey:            ENV.FIREBASE_API_KEY,
@@ -24,6 +28,7 @@ const App = StackNavigator({
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
+  MemoCreate: { screen: MemoCreateScreen },
 }, {
   navigationOptions: {
     headerTintColor: '#fff',
--- src/screens/MemoListScreen.js
+++ src/screens/MemoListScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+
 import MemoList from '../components/MemoList';
 import CircleButton from '../elements/CircleButton';
 
 class MemoListScreen extends React.Component {
+  hundlePress() {
+    const { params } = this.props.navigation.state;
+    this.props.navigation.navigate('MemoCreate', { currentUser: params.currentUser });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MemoList navigation={this.props.navigation} />
-        <CircleButton onPress={() => { this.props.navigation.navigate('MemoEdit'); }}>
+        <CircleButton onPress={this.hundlePress.bind(this)}>
           {'\uf067'}
         </CircleButton>
       </View>
